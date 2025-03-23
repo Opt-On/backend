a3253c159ac31658ea683b66468b45df0aa71afb
ericzhang11101 <ericrenzhang321@gmail.com>
2025-03-23 03:37:38 +0000

    joson parser

--- spring-boot/src/main/java/com/opton/spring_boot/controller/TranscriptController.java
+++ spring-boot/src/main/java/com/opton/spring_boot/controller/TranscriptController.java
@@ -1,6 +1,5 @@
 package com.opton.spring_boot.controller;
 
-import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.opton.spring_boot.service.TranscriptService;
 import com.opton.spring_boot.transcript_parser.TranscriptParser;
 import com.opton.spring_boot.transcript_parser.types.Summary;
+import com.opton.spring_boot.transcript_parser.types.SummaryRequest;
 
 @RestController
 @RequestMapping("/transcript")
@@ -52,15 +53,30 @@ public class TranscriptController {
 
     @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/upload/text")
-    public ResponseEntity<String> handleTextTranscriptUpload(@RequestParam("file") MultipartFile file, @RequestParam(defaultValue = "true") boolean includeGrade, @RequestParam("email") String email) {
+    public ResponseEntity<String> handleTextTranscriptUpload(@RequestParam("file") MultipartFile file, @RequestParam(defaultValue = "true") boolean includeGrade, @RequestParam("email") String email, @RequestParam("transcriptText") String text) {
         if (file.isEmpty()) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
 
         try {
-            String text = new String(file.getBytes(), StandardCharsets.UTF_8);
+            // String text = new String(file.getBytes(), StandardCharsets.UTF_8);
             Summary summary = TranscriptParser.ParseTranscriptText(text);
             transcriptService.setTranscript(summary, includeGrade, email);
+            return ResponseEntity.status(HttpStatus.OK).body("File uploaded successfully");
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PostMapping("/upload/json")
+    public ResponseEntity<String> handleJsonTranscriptUpload(@RequestBody SummaryRequest request) {
+
+        try {
+            Summary summary = request.summary;
+            String email = request.email;
+            transcriptService.setTranscript(summary, true, email);
             return ResponseEntity.status(HttpStatus.OK).body("File uploaded successfully");
         } catch (Exception e) {
             System.err.println(e.getMessage());
---
+++ spring-boot/src/main/java/com/opton/spring_boot/transcript_parser/types/SummaryRequest.java
@@ -0,0 +1,6 @@
+package com.opton.spring_boot.transcript_parser.types;
+
+public class SummaryRequest {
+    public String email;
+    public Summary summary;
+}
